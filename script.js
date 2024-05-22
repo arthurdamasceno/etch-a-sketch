07a2d307364c4d8b4d1f5d7c5b5bc74aaf64b0d6
arthurdamasceno <arthur.damasceno.gomes@hotmail.com>
2024-05-22 16:03:46 -0300

    Add hover functionfor the squares

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 let container = document.querySelector(".container");
-makeSquares();
+addFlex();
+
+function addFlex(){
+    container.style.cssText = "display: flex; flex-wrap: wrap";
+    makeSquares();
+}
 
 function makeSquares(){
     for (let i = 0; i < 16; i++){
         for (let j = 0; j < 16; j++){
             let div = document.createElement('div');
             div.classList.add("square");
+            div.addEventListener("mouseover", () => addHover(div));
             container.appendChild(div);
         }
     }
-    addFlex();
 }
 
-function addFlex(){
-    container.style.cssText = "display: flex; flex-wrap: wrap";
+function addHover(div){
+    div.classList.add("hover");
 }
-
-
-
-//div.innerHTML += '<div class="square"></div>';
