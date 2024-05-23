8853ba5bb88ece74be899a05f73c494337e07708
arthurdamasceno <arthur.damasceno.gomes@hotmail.com>
2024-05-23 20:39:38 -0300

    Add function to display squares based on selected number and add random color to hover function

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
 let container = document.querySelector(".container");
+let btn = document.querySelector("button");
+let squareNumber = 0;
 addFlex();
 
-function addFlex(){
-    container.style.cssText = "display: flex; flex-wrap: wrap";
-    makeSquares();
-}
+btn.addEventListener("click", function(e) {
+    squareNumber = getNumber();
+    makeSquares(squareNumber);
+})
 
-function makeSquares(){
-    for (let i = 0; i < 16; i++){
-        for (let j = 0; j < 16; j++){
-            let div = document.createElement('div');
-            div.classList.add("square");
-            div.addEventListener("mouseover", () => addHover(div));
-            container.appendChild(div);
-        }
+function makeSquares(squarenumber){
+    container.innerHTML = '';
+    for (let i = 0; i < (squarenumber * squareNumber); i++){
+        let div = document.createElement('div');
+        div.classList.add("square");
+        div.addEventListener("mouseover", () => addRandomColor(div));
+        setSquareSize(squarenumber, div);
+        container.appendChild(div);
     }
 }
 
-function addHover(div){
-    div.classList.add("hover");
+function addRandomColor(div){
+    let rgbColor1 = Math.floor(Math.random() * 255);
+    let rgbColor2 = Math.floor(Math.random() * 255);
+    let rgbColor3 = Math.floor(Math.random() * 255);
+    div.style.backgroundColor = `rgb(${rgbColor1}, ${rgbColor2}, ${rgbColor3})`;
+    console.log(div.style.backgroundColor)
+}
+
+function getNumber(){
+    let answer = '';
+    do {
+        answer = parseInt(prompt("How many squares per line?"));
+    } while(answer <= 0 || answer > 100);
+    return answer;
+}
+
+function setSquareSize(squarenumber, div){
+    console.log(container.style.width)
+    div.style.width = parseInt(container.style.width) / squarenumber - 2 + 'px';
+    div.style.height = div.style.width;
+    console.log(div.style.width)
+}
+
+function addFlex(){
+    container.style.cssText = "width: 900px; display: flex; flex-wrap: wrap;";
 }
